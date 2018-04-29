a6f409093c04b657e68ca89430614d21d97c9ee9
Deolin <splendid.deolin@gmail.com>
2018-04-29 09:17:19 +0800

    NPE修复

--- src/main/java/com/spldeolin/beginningmind/aspect/ControllerListener.java
+++ src/main/java/com/spldeolin/beginningmind/aspect/ControllerListener.java
@@ -3,6 +3,7 @@ package com.spldeolin.beginningmind.aspect;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.ServletRequestHandledEvent;
+import com.spldeolin.beginningmind.aspect.dto.ControllerInfo;
 import com.spldeolin.beginningmind.util.RequestContextUtil;
 import lombok.extern.log4j.Log4j2;
 
@@ -19,8 +20,12 @@ public class ControllerListener implements ApplicationListener<ServletRequestHan
 
     @Override
     public void onApplicationEvent(ServletRequestHandledEvent event) {
+        ControllerInfo controllerInfo = RequestContextUtil.getControllerInfo();
+        if (controllerInfo == null) {
+            return;
+        }
         log.info("[" + event.getMethod() + "]" + event.getRequestUrl() + SEP +
-                RequestContextUtil.getControllerInfo().getInsignia() + SEP + event.getProcessingTimeMillis());
+                controllerInfo.getInsignia() + SEP + event.getProcessingTimeMillis());
     }
 
 }
