cebc404e0c6c1c8a82936726ab541b42c551fad7
Deolin <splendid.deolin@gmail.com>
2018-05-05 15:55:29 +0800

    简化properties

--- src/main/java/com/spldeolin/beginningmind/config/BeginningMindProperties.java
+++ src/main/java/com/spldeolin/beginningmind/config/BeginningMindProperties.java
@@ -1,7 +1,5 @@
 package com.spldeolin.beginningmind.config;
 
-import java.math.BigDecimal;
-import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import lombok.Data;
@@ -20,7 +18,7 @@ public class BeginningMindProperties {
     private String oneCookie;
 
     /**
-     * 本项目的当期的地址（IP:端口号）
+     * 本项目的访问地址（IP:端口号，e.g.: http:localhot:2333）
      */
     private String address;
 
@@ -35,63 +33,9 @@ public class BeginningMindProperties {
     private String pageSizeParamName;
 
     /**
-     * IP地址
-     */
-    private String ip;
-
-    /**
-     * 版本号
-     */
-    private Integer version;
-
-    /**
-     * 价格
-     */
-    private BigDecimal price;
-
-    /**
-     * 使用一分钱支付
-     */
-    private Boolean useOneCentWhenPay;
-
-    /**
      * 文件支持
      */
     private File file;
-
-    /**
-     * “时间”格式
-     */
-    private Time time;
-
-    /**
-     * 中文文案
-     */
-    private TextZhHans textZhHans;
-
-    /**
-     * 日语文案
-     */
-    private TextJaJp textJaJp;
-
-    /**
-     * 微信
-     */
-    private Wechat wechat;
-
-    /**
-     * 阿里大鱼
-     */
-    private Alidayu alidayu;
-
-    /**
-     * 各种类型的构造体
-     */
-    private Struct struct;
-
-    /*
-     *              内部类
-     */
 
     @Data
     public static class File {
@@ -101,6 +45,11 @@ public class BeginningMindProperties {
         private String location;
 
     }
+
+    /**
+     * “时间”格式
+     */
+    private Time time;
 
     @Data
     public static class Time {
@@ -112,106 +61,6 @@ public class BeginningMindProperties {
         private String defaultDatetimePattern;
 
         private Boolean serializeJavaUtilDateToTimestamp;
-
-    }
-
-    /**
-     * 中文简体文案
-     */
-    @Data
-    public static class TextZhHans {
-
-        private String hello;
-
-        private String excellent;
-
-    }
-
-    /**
-     * 日语文案
-     */
-    @Data
-    public static class TextJaJp {
-
-        private String hello;
-
-        private String excellent;
-
-    }
-
-    /**
-     * 微信
-     */
-    @Data
-    public static class Wechat {
-
-        private String appid;
-
-        private String appsecret;
-
-    }
-
-    /**
-     * 阿里大鱼
-     */
-    @Data
-    public static class Alidayu {
-
-        private String appid;
-
-        private String appsecret;
-
-        private String signName;
-
-        private String templateCode;
-
-    }
-
-    /**
-     * 数组/Map性质的构造
-     */
-    @Data
-    public static class Struct {
-
-        /**
-         * 简单集合型构造
-         */
-        private Array array;
-
-        /**
-         * Map集合型勾结
-         */
-        private KeyValueArray keyValueArray;
-
-        @Data
-        public static class Array {
-
-            private List<String> strings;
-
-            private List<Integer> integers;
-
-        }
-
-        /**
-         * Map列表型构造
-         */
-        @Data
-        public static class KeyValueArray {
-
-            private List<CaloricFood> highCaloricFood;
-
-            private List<CaloricFood> lowCaloricFood;
-
-            @Data
-            public static class CaloricFood {
-
-                private String name;
-
-                private BigDecimal calorie;
-
-            }
-
-        }
 
     }
 
