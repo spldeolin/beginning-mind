bba7eb22fddc7580dbb237cf731042211566ee8d
Deolin <splendid.deolin@gmail.com>
2018-05-11 11:36:41 +0800

    请求执行时间打印处理从Listener转移到切面

---
+++ src/main/java/com/spldeolin/beginningmind/aspect/util/ProcessingTimeLogger.java
@@ -0,0 +1,16 @@
+package com.spldeolin.beginningmind.aspect.util;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
+
+@UtilityClass
+@Log4j2
+public class ProcessingTimeLogger {
+
+    private static final String SEP = "\t\t";
+
+    public static void logProcessingTime(String insignia, long time) {
+        log.info("[Java] 请求执行时间：" + time + SEP + insignia);
+    }
+
+}
