ddc3f35e148359c095a53b572e9d71c55938c891
Deolin <splendid.deolin@gmail.com>
2018-05-16 14:03:01 +0800

    追加NullUtils

---
+++ src/main/java/com/spldeolin/beginningmind/util/NullUtils.java
@@ -0,0 +1,34 @@
+package com.spldeolin.beginningmind.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * 将null转化为其他默认值
+ *
+ * @author Deolin
+ */
+@UtilityClass
+public class NullUtils {
+
+    public static Integer nullToZero(Integer i) {
+        if (null == i) {
+            return 0;
+        }
+        return i;
+    }
+
+    public static Long nullToZero(Long l) {
+        if (null == l) {
+            return 0L;
+        }
+        return l;
+    }
+
+    public static String nulltoEmpty(String s) {
+        if (null == s) {
+            return "";
+        }
+        return s;
+    }
+
+}
