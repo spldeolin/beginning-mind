e80e38018c839bd4be6f82a06cbae992827b441b
Deolin <splendid.deolin@gmail.com>
2018-06-08 09:31:14 +0800

    @PostConstruct的原意是用来初始化Bean的，用CommandLineRunner的派生类来进行启动环境检查更为合适

--- src/main/java/com/spldeolin/beginningmind/core/task/InitTask.java
+++ src/main/java/com/spldeolin/beginningmind/core/task/InitTask.java
@@ -1,11 +1,11 @@
 package com.spldeolin.beginningmind.core.task;
 
 import java.net.InetAddress;
-import javax.annotation.PostConstruct;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.core.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import com.spldeolin.beginningmind.core.CoreProperties;
 import com.spldeolin.beginningmind.core.service.ImageService;
@@ -20,7 +20,7 @@ import lombok.extern.log4j.Log4j2;
  */
 @Component
 @Log4j2
-public class InitTask {
+public class InitTask implements CommandLineRunner {
 
     @Value("${server.context-path}")
     private String serverContextPath;
@@ -28,10 +28,19 @@ public class InitTask {
     @Autowired
     private CoreProperties properties;
 
-    /**
-     * 将配置中出现的所有localhost替换为本地IP
-     */
-    @PostConstruct
+    @Override
+    public void run(String... args) {
+        // 将配置中出现的所有localhost替换为本地IP
+        localhostToLocalIp();
+        // 在file.mapping前追加content-path
+        mappingAppendContextPath();
+        // 确保file.mapping以 / 开头和结尾，确保file.location以 / 结尾
+        validateProperties();
+        // 确保本地目录存在
+        ensureDirectoryExist();
+        log.info("启动成功");
+    }
+
     @SneakyThrows
     public void localhostToLocalIp() {
         String address = properties.getAddress();
@@ -39,10 +48,6 @@ public class InitTask {
         properties.setAddress(address);
     }
 
-    /**
-     * 在file.mapping前追加content-path
-     */
-    @PostConstruct
     public void mappingAppendContextPath() {
         if (StringUtils.isNotBlank(serverContextPath) && !serverContextPath.equals("/")) {
             CoreProperties.File file = properties.getFile();
@@ -52,10 +57,6 @@ public class InitTask {
         }
     }
 
-    /**
-     * 确保file.mapping以 / 开头和结尾，确保file.location以 / 结尾
-     */
-    @PostConstruct
     public void validateProperties() {
         CoreProperties.File file = properties.getFile();
         if (!file.getMapping().endsWith("/")) {
@@ -69,15 +70,10 @@ public class InitTask {
         }
     }
 
-    /**
-     * 确保本地文件夹存在
-     */
-    @PostConstruct
     @SneakyThrows
     public void ensureDirectoryExist() {
         CoreProperties.File file = properties.getFile();
         FileUtils.mkdir(new java.io.File(file.getLocation() + ImageService.IMAGE_DIRECTORY), true);
         FileUtils.mkdir(new java.io.File(file.getLocation() + SignService.CAPTCHA_DIRECTORY), true);
     }
-
 }
