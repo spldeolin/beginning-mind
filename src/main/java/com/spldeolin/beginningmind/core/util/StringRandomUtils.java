db9cc7c5c7420f1d55aed5f022580e2e21492783
Deolin <splendid.deolin@gmail.com>
2018-09-08 07:01:32 +0800

    现在，StringRandomUtils不再每个方法中new char[]了

--- src/main/java/com/spldeolin/beginningmind/core/util/StringRandomUtils.java
+++ src/main/java/com/spldeolin/beginningmind/core/util/StringRandomUtils.java
@@ -1,6 +1,7 @@
 package com.spldeolin.beginningmind.core.util;
 
 import java.util.Random;
+import org.apache.commons.lang3.ArrayUtils;
 import lombok.experimental.UtilityClass;
 
 /**
@@ -8,6 +9,41 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class StringRandomUtils {
+
+    private static final char[] LOW_EN_CHARS;
+
+    private static final char[] HIGH_EN_CHARS;
+
+    private static final char[] NUM_CHARS;
+
+    private static final char[] EN_CHARS;
+
+    private static final char[] EN_NUM_CHARS;
+
+    private static final char[] LOW_EN_NUM_CHARS;
+
+    private static final char[] LEGIBLE_EN_NUM_CHARS;
+
+    static {
+        LOW_EN_CHARS = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        HIGH_EN_CHARS = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                'R',
+                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        NUM_CHARS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+        EN_CHARS = ArrayUtils.addAll(LOW_EN_CHARS, HIGH_EN_CHARS);
+
+        EN_NUM_CHARS = ArrayUtils.addAll(EN_CHARS, NUM_CHARS);
+
+        LOW_EN_NUM_CHARS = ArrayUtils.addAll(LOW_EN_CHARS, NUM_CHARS);
+
+        LEGIBLE_EN_NUM_CHARS = new char[]{'2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    }
 
     /**
      * 随机选取可见ASCII字符，生成字符串
@@ -32,10 +68,7 @@ public class StringRandomUtils {
      * @return 随机字符串
      */
     public static String generateEnNum(int stringLength) {
-        return generate(new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
-                'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-                'X', 'Y', 'Z'}, stringLength);
+        return generate(EN_NUM_CHARS, stringLength);
     }
 
     /**
@@ -45,9 +78,7 @@ public class StringRandomUtils {
      * @return 随机字符串
      */
     public static String generateLowEnNum(int stringLength) {
-        return generate(new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'},
-                stringLength);
+        return generate(LOW_EN_NUM_CHARS, stringLength);
     }
 
     /**
@@ -57,7 +88,7 @@ public class StringRandomUtils {
      * @return 随机字符串
      */
     public static String generateNum(int stringLength) {
-        return generate(new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}, stringLength);
+        return generate(NUM_CHARS, stringLength);
     }
 
     /**
@@ -67,8 +98,7 @@ public class StringRandomUtils {
      * @return 随机字符串
      */
     public static String generateLegibleEnNum(int stringLength) {
-        return generate(new char[]{'2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}, stringLength);
+        return generate(LEGIBLE_EN_NUM_CHARS, stringLength);
     }
 
     private static String generate(char[] chars, int stringLength) {
