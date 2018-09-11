90709e373f89808fd06e19e72ba83a8d815dc744
Deolin <splendid.deolin@gmail.com>
2018-09-11 14:17:50 +0800

    fix bug.

--- src/main/java/com/spldeolin/beginningmind/core/config/ScheduleConfig.java
+++ src/main/java/com/spldeolin/beginningmind/core/config/ScheduleConfig.java
@@ -1,8 +1,10 @@
 package com.spldeolin.beginningmind.core.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@Configuration
 public class ScheduleConfig {
 
 }
