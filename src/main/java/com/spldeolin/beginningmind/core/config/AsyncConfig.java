c51ae1a49b09288b2abbc083b40d8ff941ed56a1
Deolin <splendid.deolin@gmail.com>
2018-10-05 20:48:15 +0800

    追加对SpringBoot异步线程池的配置

--- src/main/java/com/spldeolin/beginningmind/core/config/AsyncConfig.java
+++ src/main/java/com/spldeolin/beginningmind/core/config/AsyncConfig.java
@@ -1,7 +1,11 @@
 package com.spldeolin.beginningmind.core.config;
 
+import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * 开启异步任务
@@ -11,5 +15,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @Configuration
 public class AsyncConfig {
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(20);
+        executor.setKeepAliveSeconds(60);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
+    }
 
 }
