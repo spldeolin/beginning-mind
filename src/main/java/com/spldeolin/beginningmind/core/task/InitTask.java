cc478234003071c866244906db3eff521dcee7f7
Deolin <splendid.deolin@gmail.com>
2018-10-07 07:15:44 +0800

    追加上传

--- src/main/java/com/spldeolin/beginningmind/core/task/InitTask.java
+++ src/main/java/com/spldeolin/beginningmind/core/task/InitTask.java
@@ -22,7 +22,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class InitTask implements CommandLineRunner {
 
-    @Value("${server.context-path}")
+    @Value("${server.contextPath}")
     private String serverContextPath;
 
     @Autowired
