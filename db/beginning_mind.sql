7505dd588cefb6a0274bb742467eeaa5f97bee8b
Deolin <splendid.deolin@gmail.com>
2018-11-22 16:37:20 +0800

    db

--- db/beginning_mind.sql
+++ db/beginning_mind.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2018-11-19 09:53:07
+Date: 2018-11-22 16:36:53
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -119,29 +119,30 @@ CREATE TABLE `user` (
   `inserted_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '通用字段 插入时间',
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '通用字段 更新时间',
   `version` int(11) NOT NULL DEFAULT '1' COMMENT '通用字段 数据版本',
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名字',
-  `mobile` char(20) NOT NULL COMMENT '手机号',
-  `email` varchar(255) NOT NULL DEFAULT '' COMMENT 'E-mail',
-  `password` char(128) NOT NULL DEFAULT '' COMMENT '密码',
-  `salt` char(32) NOT NULL DEFAULT '' COMMENT '盐',
-  `enable_sign` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '能否登录',
+  `serial_number` varchar(255) DEFAULT NULL COMMENT '用户编号',
+  `name` varchar(255) DEFAULT NULL COMMENT '名字',
+  `mobile` char(20) DEFAULT NULL COMMENT '手机号',
+  `email` varchar(255) DEFAULT NULL COMMENT 'E-mail',
+  `password` char(128) DEFAULT NULL COMMENT '密码',
+  `salt` char(32) DEFAULT NULL COMMENT '盐',
+  `enable_sign` tinyint(1) unsigned DEFAULT NULL COMMENT '能否登录',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户';
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '1970-01-01 00:00:00', '2018-08-04 17:40:44', '1', 'adam', 'admin', '', '660afa204c78e8bdebc5323e7ff9168aec159e12ce56250b6c77c220fd20d8e89a6aa382de171135a0b7aeaa28d9a0c2ca5658b5ad12fc42ad4fec6568aa0db2', 'w!-Fz?bbdWQ>[VZNu9{&jG\'Ort0s/i\"C', '1');
-INSERT INTO `user` VALUES ('2', '2018-08-07 08:33:52', '2018-08-07 08:33:52', '1', '1', '', '', '', '', '0');
-INSERT INTO `user` VALUES ('3', '2018-08-07 08:34:34', '2018-08-07 08:34:34', '1', '1', '', '', '', '', '0');
-INSERT INTO `user` VALUES ('285221975101440', '2018-11-13 08:20:56', '2018-11-19 09:51:25', '2', '乐观锁', '1', '', '', '', '0');
-INSERT INTO `user` VALUES ('287884624138240', '2018-11-13 08:31:34', '2018-11-13 08:31:28', '1', '汉字', '1', '', '', '', '0');
-INSERT INTO `user` VALUES ('288782687539200', '2018-11-13 08:35:08', '2018-11-13 08:35:01', '1', '汉字', '1', '', '', '', '0');
-INSERT INTO `user` VALUES ('289120698109952', '2018-11-13 08:36:29', '2018-11-13 14:01:50', '1', '汉字', '1', '1', '', '', '0');
-INSERT INTO `user` VALUES ('290956356227072', '2018-11-13 08:43:46', '2018-11-13 14:01:51', '1', '汉字', '1', '2', '', '', '0');
-INSERT INTO `user` VALUES ('303135042179072', '2018-11-13 09:32:10', '2018-11-13 14:01:52', '1', '汉字', '1', '2', '', '', '0');
-INSERT INTO `user` VALUES ('400214552875008', '2018-11-13 15:57:56', '2018-11-13 15:56:54', '1', '批量0', '0', '', '', '', '0');
-INSERT INTO `user` VALUES ('400214720647168', '2018-11-13 15:57:56', '2018-11-13 15:56:54', '1', '批量1', '1', '', '', '', '0');
-INSERT INTO `user` VALUES ('400214720647169', '2018-11-13 15:57:56', '2018-11-13 15:56:54', '1', '批量2', '2', '', '', '', '0');
-INSERT INTO `user` VALUES ('400214724841472', '2018-11-13 15:57:56', '2018-11-13 15:56:54', '1', '批量3', '3', '', '', '', '0');
-INSERT INTO `user` VALUES ('400214724841473', '2018-11-13 15:57:56', '2018-11-13 15:56:54', '1', '批量4', '4', '', '', '', '0');
+INSERT INTO `user` VALUES ('1', '1970-01-01 00:00:00', '2018-11-19 10:16:41', '1', '', 'adam', 'admin', '', '660afa204c78e8bdebc5323e7ff9168aec159e12ce56250b6c77c220fd20d8e89a6aa382de171135a0b7aeaa28d9a0c2ca5658b5ad12fc42ad4fec6568aa0db2', 'w!-Fz?bbdWQ>[VZNu9{&jG\'Ort0s/i\"C', '1');
+INSERT INTO `user` VALUES ('2', '2018-08-07 08:33:52', '2018-11-19 10:16:43', '1', '', '1', '', '', '', '', '0');
+INSERT INTO `user` VALUES ('3', '2018-08-07 08:34:34', '2018-11-19 10:16:43', '1', '', '1', '', '', '', '', '0');
+INSERT INTO `user` VALUES ('285221975101440', '2018-11-13 08:20:56', '2018-11-19 10:16:43', '2', '', '乐观锁', '1', '', '', '', '0');
+INSERT INTO `user` VALUES ('287884624138240', '2018-11-13 08:31:34', '2018-11-19 10:16:43', '1', '', '汉字', '1', '', '', '', '0');
+INSERT INTO `user` VALUES ('288782687539200', '2018-11-13 08:35:08', '2018-11-19 10:16:43', '1', '', '汉字', '1', '', '', '', '0');
+INSERT INTO `user` VALUES ('289120698109952', '2018-11-13 08:36:29', '2018-11-19 10:16:43', '1', '', '汉字', '1', '1', '', '', '0');
+INSERT INTO `user` VALUES ('290956356227072', '2018-11-13 08:43:46', '2018-11-19 10:16:43', '1', '', '汉字', '1', '2', '', '', '0');
+INSERT INTO `user` VALUES ('303135042179072', '2018-11-13 09:32:10', '2018-11-19 10:16:43', '1', '', '汉字', '1', '2', '', '', '0');
+INSERT INTO `user` VALUES ('400214552875008', '2018-11-13 15:57:56', '2018-11-19 10:32:09', '1', '', 'a1a@a', '0', '', '', '', '0');
+INSERT INTO `user` VALUES ('400214720647168', '2018-11-13 15:57:56', '2018-11-19 10:32:34', '1', '', '批量1', '1111', '', '', '', '0');
+INSERT INTO `user` VALUES ('400214720647169', '2018-11-13 15:57:56', '2018-11-19 10:32:38', '1', '', '批量2', '2', 'aa@a', '', '', '0');
+INSERT INTO `user` VALUES ('400214724841472', '2018-11-13 15:57:56', '2018-11-19 10:16:43', '1', '', '批量3', '3', '', '', '', '0');
+INSERT INTO `user` VALUES ('400214724841473', '2018-11-13 15:57:56', '2018-11-19 10:16:43', '1', '', '批量4', '4', '', '', '', '0');
