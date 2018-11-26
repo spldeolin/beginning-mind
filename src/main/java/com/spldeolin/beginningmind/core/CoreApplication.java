9f8773ee3a0df12002555ba9348b073309087ef5
Deolin <splendid.deolin@gmail.com>
2018-11-26 15:18:37 +0800

    简化日志输出，非prod环境只打印com.splendid包下类的日志

--- src/main/java/com/spldeolin/beginningmind/core/CoreApplication.java
+++ src/main/java/com/spldeolin/beginningmind/core/CoreApplication.java
@@ -3,17 +3,19 @@ package com.spldeolin.beginningmind.core;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
+@Log4j2
 public class CoreApplication {
 
     public static void main(String[] args) {
         if (!StringUtils.equalsAny(System.getProperty("profile"), "dev", "test", "prod")) {
-            System.out.println("没有正确指定-Dprofile，启动被阻止。");
-            return;
+            log.fatal("没有正确指定-Dprofile，启动被阻止。");
+            System.exit(0);
         }
+
         SpringApplication.run(CoreApplication.class, args);
-        System.out.println("beginning-mind启动成功");
     }
 
 }
