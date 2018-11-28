aff0cf09e325b00d05bce14ae0198bc691f9605d
Deolin <splendid.deolin@gmail.com>
2018-11-28 16:52:13 +0800

    i18n

---
+++ src/main/java/com/spldeolin/beginningmind/core/config/I18nConfig.java
@@ -0,0 +1,22 @@
+package com.spldeolin.beginningmind.core.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * @author beingmate2016 2018/11/28
+ */
+@Configuration
+public class I18nConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
--- src/main/java/com/spldeolin/beginningmind/core/controller/TestController.java
+++ src/main/java/com/spldeolin/beginningmind/core/controller/TestController.java
@@ -2,6 +2,7 @@ package com.spldeolin.beginningmind.core.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.spldeolin.beginningmind.core.model.User;
 import com.spldeolin.beginningmind.core.service.UserService;
+import com.spldeolin.beginningmind.core.util.Requests;
 
 /**
  * @author Deolin 2018/11/16
@@ -30,6 +32,14 @@ public class TestController {
     @PostMapping("/s")
     List<User> ln23(User user) {
         return userService.listAll();
+    }
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/i18n")
+    Object i18n() {
+        return messageSource.getMessage("hello", new String[]{"Deolin"}, Requests.request().getLocale());
     }
 
 }
