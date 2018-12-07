53d143c6e8ef51a8c3c2c4dd0449086c95531c36
Deolin <splendid.deolin@gmail.com>
2018-12-07 09:27:25 +0800

    CommonFieldFillHandler的注册移动到MybatisplusConfig中

--- src/main/java/com/spldeolin/beginningmind/core/config/MybatisPlusConfig.java
+++ src/main/java/com/spldeolin/beginningmind/core/config/MybatisPlusConfig.java
@@ -1,11 +1,16 @@
 package com.spldeolin.beginningmind.core.config;
 
+import java.time.LocalDateTime;
+import org.apache.ibatis.reflection.MetaObject;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.spldeolin.beginningmind.core.service.SnowFlakeService;
 
 /**
  * @author Deolin 2018/11/10
@@ -13,6 +18,9 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 @MapperScan("com.spldeolin.beginningmind.core.dao")
 @Configuration
 public class MybatisPlusConfig {
+
+    @Autowired
+    private SnowFlakeService snowFlakeService;
 
     /**
      * 启用乐观锁
@@ -37,5 +45,38 @@ public class MybatisPlusConfig {
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
+
+    @Bean
+    public MetaObjectHandler commonFieldFillHandler() {
+        return new CommonFieldFillHandler(snowFlakeService);
+    }
+
+    private static class CommonFieldFillHandler implements MetaObjectHandler {
+
+        private SnowFlakeService snowFlakeService;
+
+        private CommonFieldFillHandler(SnowFlakeService snowFlakeService) {
+            this.snowFlakeService = snowFlakeService;
+        }
+
+        @Override
+        public void insertFill(MetaObject metaObject) {
+            // 使用雪花算法生成ID
+            this.setFieldValByName("id", snowFlakeService.nextId(), metaObject);
+            // 使用当前时间
+            LocalDateTime now = LocalDateTime.now();
+            this.setFieldValByName("insertedAt", now, metaObject);
+            this.setFieldValByName("updatedAt", now, metaObject);
+        }
+
+        @Override
+        public void updateFill(MetaObject metaObject) {
+            // 使用当前时间
+            LocalDateTime now = LocalDateTime.now();
+            this.setFieldValByName("updatedAt", now, metaObject);
+        }
+
+    }
+
 
 }
