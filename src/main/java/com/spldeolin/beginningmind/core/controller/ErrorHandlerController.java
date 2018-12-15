3986f9860d6850212a0ff2d51529c21cf0934a75
Deolin <splendid.deolin@gmail.com>
2018-12-15 19:46:08 +0800

    追加ErrorHandlerController

---
+++ src/main/java/com/spldeolin/beginningmind/core/controller/ErrorHandlerController.java
@@ -0,0 +1,41 @@
+package com.spldeolin.beginningmind.core.controller;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ * 用于统一返回无法被ExceptionAdvance捕获的外层异常（这些往往是在过滤器中抛出的异常）
+ *
+ * @author Administrator 2018/12/15
+ */
+@RestController
+public class ErrorHandlerController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @RequestMapping(value = PATH)
+    Map<String, Object> error(HttpServletRequest request) {
+        return getErrorAttributes(request);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request) {
+        WebRequest webRequest = new ServletWebRequest(request);
+        return errorAttributes.getErrorAttributes(webRequest, true);
+    }
+
+}
