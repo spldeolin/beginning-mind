7a05765c0c52936934aea663be995d52c2c9e009
Deolin <splendid.deolin@gmail.com>
2018-12-29 11:08:08 +0800

    轻微修正

--- src/main/java/com/spldeolin/beginningmind/core/util/Https.java
+++ src/main/java/com/spldeolin/beginningmind/core/util/Https.java
@@ -35,11 +35,7 @@ public class Https {
     private static final String DISGUISED_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
             + " (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
 
-    private static OkHttpClient client;
-
-    static {
-        client = new OkHttpClient();
-    }
+    private static OkHttpClient client = new OkHttpClient();
 
     /**
      * 发送一个GET请求，获取JSON
