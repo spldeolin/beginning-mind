724731835784c78c60ce9d5177347564e40ac288
Deolin <splendid.deolin@gmail.com>
2019-01-15 14:16:14 +0800

    回滚

---
+++ src/main/java/com/spldeolin/beginningmind/core/mvc/RequestResultWrapHandlerFactoryBean.java
@@ -0,0 +1,41 @@
+package com.spldeolin.beginningmind.core.mvc;
+
+import java.util.List;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
+import com.google.common.collect.Lists;
+import com.spldeolin.beginningmind.core.util.Nulls;
+
+/**
+ * @author Deolin
+ */
+@Component
+public class RequestResultWrapHandlerFactoryBean implements InitializingBean {
+
+    @Autowired
+    private RequestMappingHandlerAdapter adapter;
+
+    @Override
+    public void afterPropertiesSet() {
+        List<HandlerMethodReturnValueHandler> returnValueHandlers = Lists
+                .newArrayList(Nulls.toEmpty(adapter.getReturnValueHandlers()));
+        decorateHandlers(returnValueHandlers);
+        adapter.setReturnValueHandlers(returnValueHandlers);
+    }
+
+    private void decorateHandlers(List<HandlerMethodReturnValueHandler> handlers) {
+        for (HandlerMethodReturnValueHandler handler : handlers) {
+            if (handler instanceof RequestResponseBodyMethodProcessor) {
+                RequestResultWrapHandler decorator = new RequestResultWrapHandler(handler);
+                int index = handlers.indexOf(handler);
+                handlers.set(index, decorator);
+                break;
+            }
+        }
+    }
+
+}
