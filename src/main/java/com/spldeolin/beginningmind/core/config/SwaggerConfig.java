a9eb560758e733d656c5e735c233ec114398df8a
Deolin <splendid.deolin@gmail.com>
2019-01-22 09:44:50 +0800

    集成Swagger Starter

---
+++ src/main/java/com/spldeolin/beginningmind/core/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.spldeolin.beginningmind.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import com.spring4all.swagger.EnableSwagger2Doc;
+
+/**
+ * 启用Swagger文档
+ *
+ * @author Deolin 2019-01-21
+ */
+@EnableSwagger2Doc
+@Configuration
+public class SwaggerConfig {
+
+}
