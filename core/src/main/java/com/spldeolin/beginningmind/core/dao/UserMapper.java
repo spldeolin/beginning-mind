a15603fcf34931c0729f0199915af810206cd937
Deolin <splendid.deolin@gmail.com>
2019-03-31 10:06:39 +0800

    分页演示

--- core/src/main/java/com/spldeolin/beginningmind/core/dao/UserMapper.java
+++ core/src/main/java/com/spldeolin/beginningmind/core/dao/UserMapper.java
@@ -1,9 +1,8 @@
 package com.spldeolin.beginningmind.core.dao;
 
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.spldeolin.beginningmind.core.entity.UserEntity;
 
@@ -14,7 +13,7 @@ import com.spldeolin.beginningmind.core.entity.UserEntity;
  */
 public interface UserMapper extends BaseMapper<UserEntity> {
 
-    @Select("select * from user")
-    List<UserEntity> pages(@Param("page") Page<UserEntity> param);
+//    @Select("select * from user where is_deleted = false and mobile = #{mobile}")
+    IPage<UserEntity> searchAsPageByMobile(Page<UserEntity> param, @Param("mobile") String mobile);
 
 }
