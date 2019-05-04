b81609bc8f3a7ee45d86053d9e91437da269d03c
Deolin <splendid.deolin@gmail.com>
2019-05-04 17:02:42 +0800

    为了确保API安全性，通过拦截返回值的形式，让调用方无法通过返回值判断请求是否Not Found

--- core/src/main/java/com/spldeolin/beginningmind/core/mvc/RequestResultWrapHandler.java
+++ core/src/main/java/com/spldeolin/beginningmind/core/mvc/RequestResultWrapHandler.java
@@ -1,14 +1,20 @@
 package com.spldeolin.beginningmind.core.mvc;
 
+import java.util.LinkedHashMap;
+import java.util.Objects;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import com.spldeolin.beginningmind.core.aspect.dto.RequestResult;
+import com.spldeolin.beginningmind.core.constant.ResultCode;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * @author Deolin 2018/11/16
  */
+@Log4j2
 public class RequestResultWrapHandler implements HandlerMethodReturnValueHandler {
 
     private final HandlerMethodReturnValueHandler delegate;
@@ -26,7 +32,21 @@ public class RequestResultWrapHandler implements HandlerMethodReturnValueHandler
     public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer,
             NativeWebRequest webRequest) throws Exception {
         RequestResult result = ensureWrapped(returnValue);
+
+        if (is404(returnValue)) {
+            log.warn("请求404");
+            result = RequestResult.failure(ResultCode.BAD_REQEUST);
+        }
+
         delegate.handleReturnValue(result, returnType, mavContainer, webRequest);
+    }
+
+    private boolean is404(Object returnValue) {
+        if (returnValue instanceof LinkedHashMap) {
+            Object status = ((LinkedHashMap) returnValue).get("status");
+            return Objects.equals(status, HttpStatus.NOT_FOUND.value());
+        }
+        return false;
     }
 
     private RequestResult ensureWrapped(Object returnValue) {
