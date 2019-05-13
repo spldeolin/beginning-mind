9aa3a6611566274944bc24443252e036032d1883
Deolin <splendid.deolin@gmail.com>
2019-05-13 09:41:50 +0800

    基于切面，实现了定时任务MDC

--- core/src/main/java/com/spldeolin/beginningmind/core/TimeTask.java
+++ core/src/main/java/com/spldeolin/beginningmind/core/TimeTask.java
@@ -1,10 +1,8 @@
 package com.spldeolin.beginningmind.core;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.ThreadContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import com.spldeolin.beginningmind.core.util.StringRandomUtils;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -14,13 +12,17 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class TimeTask {
 
-    //    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 5000)
     public void report() {
-        ThreadContext.put("async", "[TT-" + StringRandomUtils.generateLegibleEnNum(3) + "]");
-
-
+        log.info(ThreadContext.get("insignia"));
         log.info("啊");
-        ThreadContext.remove("async");
+        log.info("啊");
+        log.info("啊");
+        log.info("啊");
+        log.info("啊");
+        log.info("啊");
+        log.info("啊");
+        log.info("啊");
     }
 
 }
