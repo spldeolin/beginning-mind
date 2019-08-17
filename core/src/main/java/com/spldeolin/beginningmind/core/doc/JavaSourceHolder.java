35bfd448cb237decd9a7bb6619cd7b3c5629ebb5
Deolin <splendid.deolin@gmail.com>
2019-08-17 20:49:30 +0800

    采用基于qdox解析JavaDoc的方式 代替 植入springfox注解对Api文档进行注释，避免代码污染（作成中）

--- core/src/main/java/com/spldeolin/beginningmind/core/doc/JavaSourceHolder.java
+++ core/src/main/java/com/spldeolin/beginningmind/core/doc/JavaSourceHolder.java
@@ -3,14 +3,20 @@ package com.spldeolin.beginningmind.core.doc;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Table;
 import com.thoughtworks.qdox.JavaProjectBuilder;
 import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaMethod;
 import com.thoughtworks.qdox.model.JavaSource;
+import com.thoughtworks.qdox.model.JavaType;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -24,6 +30,10 @@ public class JavaSourceHolder {
 
     private final Map<String, JavaClass> controllerSources = Maps.newHashMap();
 
+    /**
+     * 方法的全限定名：方法每个参数的类型的全限定名：方法注释
+     */
+    private final Table<String, List<String>, String> methodCommentMap = HashBasedTable.create();
 
     private final JavaProjectBuilder builder = new JavaProjectBuilder();
 
@@ -42,6 +52,20 @@ public class JavaSourceHolder {
                     if (JavaClassVisitor.isController(javaClass)) {
                         controllerSources.put(fqName, javaClass);
                     }
+
+                    // extract method comment
+                    for (JavaMethod javaMethod : javaClass.getMethods()) {
+                        String methodFqName = fqName + "." + javaMethod.getName();
+                        List<String> parameterFqNames = Lists.newArrayList();
+                        for (JavaType javaType : javaMethod.getParameterTypes()) {
+                            parameterFqNames.add(javaType.getFullyQualifiedName());
+                        }
+                        String comment = "";
+                        if (javaMethod.getComment() != null) {
+                            comment = javaMethod.getComment();
+                        }
+                        methodCommentMap.put(methodFqName, parameterFqNames, comment);
+                    }
                 }
             } catch (IOException e) {
                 log.info(e);
@@ -51,6 +75,10 @@ public class JavaSourceHolder {
 
     public JavaClass getSource(String fullyQualifiedName) {
         return classSources.get(fullyQualifiedName);
+    }
+
+    public String getMethodComment(String methodFullyQualifiedName, List<String> parameterTypeFullQulifiedNames) {
+        return methodCommentMap.get(methodFullyQualifiedName, parameterTypeFullQulifiedNames);
     }
 
 }
