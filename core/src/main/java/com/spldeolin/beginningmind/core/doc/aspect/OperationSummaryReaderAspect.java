9c666b97a67ff63883f2db96adc26a631ef766e7
Deolin <splendid.deolin@gmail.com>
2019-08-17 21:13:35 +0800

    采用基于qdox解析JavaDoc的方式 代替 植入springfox注解对Api文档进行注释，避免代码污染（作成中）

--- core/src/main/java/com/spldeolin/beginningmind/core/doc/aspect/OperationSummaryReaderAspect.java
+++ core/src/main/java/com/spldeolin/beginningmind/core/doc/aspect/OperationSummaryReaderAspect.java
@@ -1,15 +1,20 @@
 package com.spldeolin.beginningmind.core.doc.aspect;
 
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Objects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.web.method.HandlerMethod;
+import com.google.common.collect.Lists;
 import com.spldeolin.beginningmind.core.doc.JavaSourceHolder;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.spi.service.contexts.ApiListingContext;
 import springfox.documentation.spi.service.contexts.OperationContext;
 import springfox.documentation.spi.service.contexts.RequestMappingContext;
@@ -35,14 +40,27 @@ public class OperationSummaryReaderAspect {
 
     @Around("apply()")
     public void around(ProceedingJoinPoint point) throws Throwable {
-        OperationContext operation = (OperationContext) point.getArgs()[0];
+        OperationContext context = (OperationContext) point.getArgs()[0];
 
-        Field field = Objects.requireNonNull(ReflectionUtils.findField(OperationContext.class, "requestContext"));
-        field.setAccessible(true);
-        RequestMappingContext req = (RequestMappingContext) ReflectionUtils.getField(field, operation);
+        RequestMappingContext requestMappingContext = getFieldValue(OperationContext.class, context, "requestContext");
+        RequestHandler requestHandler = getFieldValue(RequestMappingContext.class, requestMappingContext, "handler");
+        HandlerMethod handlerMethod = requestHandler.getHandlerMethod();
+        String methodFqName = handlerMethod.getBeanType().getName() + "." + handlerMethod.getMethod().getName();
+        List<String> paramFqNames = Lists.newArrayList();
+        for (MethodParameter methodParameter : handlerMethod.getMethodParameters()) {
+            paramFqNames.add(methodParameter.getParameterType().getName());
+        }
 
         point.proceed(point.getArgs());
-        // TODO 获取req关于这个handler的全限定名+参数类型列表（考虑方法重载），通过获取到的组合信息，从srcHolder获取唯一的那个JavaMethod
+
+        context.operationBuilder().summary(srcHolder.getMethodComment(methodFqName, paramFqNames));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T getFieldValue(Class<?> targetType, Object target, String fieldName) {
+        Field field = Objects.requireNonNull(ReflectionUtils.findField(targetType, fieldName));
+        field.setAccessible(true);
+        return (T) ReflectionUtils.getField(field, target);
     }
 
 }
