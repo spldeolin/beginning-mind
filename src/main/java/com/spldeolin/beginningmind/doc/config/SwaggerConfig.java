587dfa81c2d0230719665325752ebd0fcc3995b2
Deolin <splendid.deolin@gmail.com>
2019-09-26 13:48:32 +0800

    code reformat

--- src/main/java/com/spldeolin/beginningmind/doc/config/SwaggerConfig.java
+++ src/main/java/com/spldeolin/beginningmind/doc/config/SwaggerConfig.java
@@ -1,12 +1,11 @@
 package com.spldeolin.beginningmind.doc.config;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.google.common.collect.Lists;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
@@ -22,17 +21,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class SwaggerConfig {
 
+    // @formatter:off
     @Bean
-    public Docket createRestApi() {
-        ParameterBuilder ticketPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        ticketPar.name("sessionId").description("TOKEN").modelRef(new ModelRef("string")).parameterType("header")
-                .required(false).build();
-        pars.add(ticketPar.build());
+    public Docket defaultDocket() {
 
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.spldeolin.beginningmind")).paths(PathSelectors.any())
-                .build().globalOperationParameters(pars);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("default") // /v2/api-docs?group=
+                .useDefaultResponseMessages(true) // "Responses"下只显示Code为200的情况
+                .globalOperationParameters(commonHeaders()) // 指定通用headers
+                .select()
+                .paths(PathSelectors.ant("/**"))    // 请求url
+                .build();
+    }
+    // @formatter:on
+
+    private List<Parameter> commonHeaders() {
+        return Lists.newArrayList(
+                new ParameterBuilder().name("token").description("登录态TOKEN").modelRef(new ModelRef("string"))
+                        .parameterType("header").required(false).build());
     }
 
 }
