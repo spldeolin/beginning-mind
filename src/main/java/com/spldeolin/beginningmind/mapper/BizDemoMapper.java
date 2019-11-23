26927f41aee3bb761744c26b79f081d9843b03b2
Deolin <splendid.deolin@gmail.com>
2019-11-24 07:56:29 +0800

    fix bug

--- src/main/java/com/spldeolin/beginningmind/mapper/BizDemoMapper.java
+++ src/main/java/com/spldeolin/beginningmind/mapper/BizDemoMapper.java
@@ -1,10 +1,7 @@
 package com.spldeolin.beginningmind.mapper;
 
-import java.util.List;
 import java.util.Map;
 import org.apache.ibatis.annotations.MapKey;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.ResultType;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.spldeolin.beginningmind.entity.BizDemoEntity;
 
@@ -15,7 +12,7 @@ import com.spldeolin.beginningmind.entity.BizDemoEntity;
  */
 public interface BizDemoMapper extends BaseMapper<BizDemoEntity> {
 
-    @MapKey("name")
+    @MapKey("id")
     Map<Long, BizDemoEntity> listAllAsMap();
 
 }
