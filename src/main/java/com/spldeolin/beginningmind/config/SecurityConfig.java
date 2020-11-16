107833cc289a492603e050b7d93f96a451b344cc
Deolin <splendid.deolin@gmail.com>
2020-11-16 11:25:51 +0800

    删除不必要的注释

--- src/main/java/com/spldeolin/beginningmind/config/SecurityConfig.java
+++ src/main/java/com/spldeolin/beginningmind/config/SecurityConfig.java
@@ -9,11 +9,6 @@ import com.spldeolin.beginningmind.service.SecurityAccessTokenService;
 /**
  * 安全配置
  *
- * <pre>
- * 由于自Spring Boot 2.1.5起，项目必须依赖spring-boot-starter-security，否则自动报错，
- * 所以只能引入Spring Security，并通过配置禁用掉Spring Security。
- * </pre>
- *
  * @author Deolin 2018/12/02
  */
 @Configuration
