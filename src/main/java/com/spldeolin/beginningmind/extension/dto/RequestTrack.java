35146f0fa71ec402906c914b4f9f8d829b0e430a
Deolin <splendid.deolin@gmail.com>
2020-11-22 10:27:07 +0800

    RequestTrack：使用alibaba ttl作为ThreadLocal context的实现。https://github.com/alibaba/transmittable-thread-local/

--- src/main/java/com/spldeolin/beginningmind/extension/dto/RequestTrack.java
+++ src/main/java/com/spldeolin/beginningmind/extension/dto/RequestTrack.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
+import com.alibaba.ttl.TransmittableThreadLocal;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.spldeolin.beginningmind.util.JsonUtils;
 import lombok.Data;
@@ -19,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RequestTrack {
 
-    private static final ThreadLocal<RequestTrack> context = ThreadLocal.withInitial(RequestTrack::new);
+    private static final ThreadLocal<RequestTrack> context = TransmittableThreadLocal.withInitial(RequestTrack::new);
 
     private RequestTrack() {
     }
