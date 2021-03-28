042582cd095c145c441dcdd130b9ade4792ea1c8
Deolin <splendid.deolin@gmail.com>
2021-03-28 12:29:54 +0800

    使用Maven Profile中的属性作为spring.profiles.active的来源值，简化应用的profile的机制

--- src/main/java/com/spldeolin/beginningmind/Application.java
+++ src/main/java/com/spldeolin/beginningmind/Application.java
@@ -1,6 +1,5 @@
 package com.spldeolin.beginningmind;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,11 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
-        if (!StringUtils.equalsAny(System.getProperty("profile"), "dev", "test", "prod")) {
-            System.out.println("没有正确地指定-Dprofile，启动被阻止。");
-            return;
-        }
-
         SpringApplication.run(Application.class, args);
     }
 
