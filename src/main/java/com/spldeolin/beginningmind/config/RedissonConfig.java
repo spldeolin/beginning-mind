001e4f9164ee669dbbfcf3bc7026d47941c763ae
Deolin <splendid.deolin@gmail.com>
2023-04-02 11:08:27 +0800

    OPTIM: 使用FST代替Jackson作为Redisson Codec，因为前者在序列化/反序列化对象时，会受到Jackson注解的影响，而这些注解往往是在别的场景使用，并不希望影响到Redisson的序列化，所以使用FST代替Jackson

--- src/main/java/com/spldeolin/beginningmind/config/RedissonConfig.java
+++ src/main/java/com/spldeolin/beginningmind/config/RedissonConfig.java
@@ -1,11 +1,10 @@
 package com.spldeolin.beginningmind.config;
 
 import org.redisson.api.RedissonClient;
-import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.codec.FstCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -18,14 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 public class RedissonConfig {
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
     private RedissonClient redissonClient;
 
     @Bean
     public void redissonClient() {
-        redissonClient.getConfig().setCodec(new JsonJacksonCodec(objectMapper));
+        redissonClient.getConfig().setCodec(new FstCodec());
     }
 
 }
