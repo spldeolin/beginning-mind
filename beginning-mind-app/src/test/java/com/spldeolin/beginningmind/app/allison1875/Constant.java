aebeae9ff6e86f89ccd1ac4c04762001c9ac27cf
Deolin <splendid.deolin@gmail.com>
2024-02-16 19:43:59 +0800

    BUMP: allison1875 from 8.8 to 10.1-SNAPSHOT

---
+++ beginning-mind-app/src/test/java/com/spldeolin/beginningmind/app/allison1875/Constant.java
@@ -0,0 +1,23 @@
+package com.spldeolin.beginningmind.app.allison1875;
+
+import com.google.common.collect.Lists;
+import com.spldeolin.allison1875.common.config.CommonConfig;
+
+/**
+ * @author Deolin 2024-02-19
+ */
+public interface Constant {
+
+    CommonConfig COMMON_CONFIG = new CommonConfig().setReqDtoPackage("com.spldeolin.beginningmind.app.javabean.req")
+            .setRespDtoPackage("com.spldeolin.beginningmind.app.javabean.resp")
+            .setServicePackage("com.spldeolin.beginningmind.app.service")
+            .setServiceImplPackage("com.spldeolin.beginningmind.app.service.impl")
+            .setMapperPackage("com.spldeolin.beginningmind.app.mapper")
+            .setEntityPackage("com.spldeolin.beginningmind.app.entity")
+            .setDesignPackage("com.spldeolin.beginningmind.app.design")
+            .setCondPackage("com.spldeolin.beginningmind.app.javabean.cond")
+            .setRecordPackage("com.spldeolin.beginningmind.app.javabean.record")
+            .setWholeDtoPackage("com.spldeolin.beginningmind.app.javabean")
+            .setMapperXmlDirectoryPaths(Lists.newArrayList("src/main/resources/mapper")).setAuthor("Deolin");
+
+}
---
+++ beginning-mind-app/src/test/java/com/spldeolin/beginningmind/app/allison1875/PersistenceGeneratorBoot.java
@@ -0,0 +1,77 @@
+package com.spldeolin.beginningmind.app.allison1875;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+import com.google.common.collect.Lists;
+import com.spldeolin.allison1875.common.Allison1875;
+import com.spldeolin.allison1875.common.enums.FileExistenceResolutionEnum;
+import com.spldeolin.allison1875.persistencegenerator.PersistenceGeneratorConfig;
+import com.spldeolin.allison1875.persistencegenerator.PersistenceGeneratorModule;
+import com.spldeolin.allison1875.persistencegenerator.service.CommentService;
+import com.spldeolin.allison1875.persistencegenerator.service.JdbcTypeService;
+import com.spldeolin.beginningmind.app.Application;
+import com.spldeolin.beginningmind.app.allison1875.serviceimpl.persistencegenerator.CommentServiceImpl2;
+import com.spldeolin.beginningmind.app.allison1875.serviceimpl.persistencegenerator.JdbcTypeServiceImpl2;
+
+/**
+ * @author Deolin 2020-11-17
+ */
+public class PersistenceGeneratorBoot {
+
+    public static void main(String[] args) throws IOException {
+        List<String> dbConfig = FileUtils.readLines(new File("/Users/deolin/Documents/temp-db.txt"),
+                StandardCharsets.UTF_8);
+
+        PersistenceGeneratorConfig config = new PersistenceGeneratorConfig();
+        config.setCommonConfig(Constant.COMMON_CONFIG);
+        config.setJdbcUrl("jdbc:mysql://" + dbConfig.get(0));
+        config.setUserName(dbConfig.get(1));
+        config.setPassword(dbConfig.get(2));
+        config.setSchema(dbConfig.get(3));
+        config.setTables(Lists.newArrayList());
+        config.setEnableGenerateDesign(true);
+        config.setIsEntityUsingAlias(false);
+        config.setIsEntityEndWithEntity(true);
+        config.setDeletedSql(null);
+        config.setNotDeletedSql(null);
+        config.setHiddenColumns(Lists.newArrayList());
+        config.setNotKeyColumns(Lists.newArrayList());
+        config.setSuperEntityQualifier(null);
+        config.setAlreadyInSuperEntity(Lists.newArrayList());
+        config.setEnableNoModifyAnnounce(true);
+        config.setEnableLotNoAnnounce(false);
+        config.setEnableEntityImplementSerializable(true);
+        config.setEnableEntityImplementCloneable(true);
+        config.setEntityExistenceResolution(FileExistenceResolutionEnum.OVERWRITE);
+        config.setDisableInsert(false);
+        config.setDisableBatchInsert(false);
+        config.setDisableBatchInsertEvenNull(false);
+        config.setDisableBatchUpdate(false);
+        config.setDisableBatchUpdateEvenNull(false);
+        config.setDisableQueryById(false);
+        config.setDisableUpdateById(false);
+        config.setDisableUpdateByIdEvenNull(false);
+        config.setDisableQueryByIds(false);
+        config.setDisableQueryByIdsEachId(false);
+        config.setDisableQueryByKey(false);
+        config.setDisableDeleteByKey(false);
+        config.setDisableQueryByKeys(false);
+        config.setDisableQueryByEntity(false);
+        config.setDisableListAll(false);
+        config.setDisableInsertOrUpdate(false);
+
+        Allison1875.allison1875(Application.class, new PersistenceGeneratorModule(config) {
+            @Override
+            protected void configure() {
+                super.configure();
+                bind(JdbcTypeService.class).toInstance(
+                        new JdbcTypeServiceImpl2("com.spldeolin.beginningmind.app.enums"));
+                bind(CommentService.class).toInstance(new CommentServiceImpl2());
+            }
+        });
+    }
+
+}
